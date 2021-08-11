50d60d347899201e36d18e321409d56c5f056858
Alexandre Drouin <alexandre.drouin1@gmail.com>
2021-08-11 15:20:56 +0000

    Added license headers

--- gene_classification/data/data_utils.py
+++ gene_classification/data/data_utils.py
@@ -1,5 +1,20 @@
 """
-Utility functions for data
+Copyright 2021 ServiceNow
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+    
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+
+Utility functions for genomic data
 
 """
 import gzip
@@ -12,7 +27,22 @@ from itertools import product
 
 # Constants for handling non-standard nucleotides
 NON_STANDARD_MARKER = "*"
-NON_STANDARD_NUCLEOTIDES = ["k", "m", "r", "y", "s", "w", "b", "v", "h", "d", "x", "n", "." , "-"]
+NON_STANDARD_NUCLEOTIDES = [
+    "k",
+    "m",
+    "r",
+    "y",
+    "s",
+    "w",
+    "b",
+    "v",
+    "h",
+    "d",
+    "x",
+    "n",
+    ".",
+    "-",
+]
 
 
 def contig_flag_non_standard(s, replace_by=NON_STANDARD_MARKER):
@@ -66,25 +96,29 @@ def hash_string(sequence, k=3, bins=10, kmer_weights=None):
         raise ValueError("The sequence contains non-standard nucleotides.")
 
     if kmer_weights is None:
-        kmer_weights = defaultdict(lambda: 1.)
+        kmer_weights = defaultdict(lambda: 1.0)
 
-    n_kmers = 4**k
+    n_kmers = 4 ** k
     if bins >= n_kmers:
         # We don't need a hashing function, there are enough bins
         alphabet = ["a", "c", "g", "t"]
-        kmer_to_idx = {"".join(x): y for x, y in zip(list(product(*([alphabet] * k))), range(n_kmers))}
+        kmer_to_idx = {
+            "".join(x): y
+            for x, y in zip(list(product(*([alphabet] * k))), range(n_kmers))
+        }
+
         def kmer_hasher(kmer):
             return kmer_to_idx[kmer]
+
     else:
         kmer_hasher = hash
 
     a = np.zeros(bins)
-    n_valid_kmers = 0.
+    n_valid_kmers = 0.0
     for i in range(len(sequence)):
         kmer = sequence[i : i + k]
         # Skip k-mers that contain flagged non-standard nucleotides or that are too short
-        if len(kmer) < k or \
-           NON_STANDARD_MARKER in kmer:
+        if len(kmer) < k or NON_STANDARD_MARKER in kmer:
             continue
         a[abs(kmer_hasher(kmer)) % bins] += kmer_weights[kmer]
         n_valid_kmers += 1
@@ -102,31 +136,34 @@ def one_hot_dna(sequence):
         raise ValueError("The sequence contains invalid nucleotides.")
 
     if NON_STANDARD_MARKER in sequence:
-        raise ValueError("The sequence to one-hot encode should not contain invalid nucleotide flags.")
+        raise ValueError(
+            "The sequence to one-hot encode should not contain invalid nucleotide flags."
+        )
 
     if len(sequence) == 0:
         return np.array([]).reshape(0, 4)
 
-    convert = {"a": np.array([1, 0, 0, 0], dtype=np.float),
-               "t": np.array([0, 1, 0, 0], dtype=np.float),
-               "u": np.array([0, 1, 0, 0], dtype=np.float),
-               "c": np.array([0, 0, 1, 0], dtype=np.float),
-               "g": np.array([0, 0, 0, 1], dtype=np.float),
-               "k": np.array([0, 1, 0, 1], dtype=np.float) / 2.,
-               "m": np.array([1, 0, 1, 0], dtype=np.float) / 2.,
-               "r": np.array([1, 0, 0, 1], dtype=np.float) / 2.,
-               "y": np.array([0, 1, 1, 0], dtype=np.float) / 2.,
-               "s": np.array([0, 0, 1, 1], dtype=np.float) / 2.,
-               "w": np.array([1, 1, 0, 0], dtype=np.float) / 2.,
-               "b": np.array([0, 1, 1, 1], dtype=np.float) / 3.,
-               "v": np.array([1, 0, 1, 1], dtype=np.float) / 3.,
-               "h": np.array([1, 1, 1, 0], dtype=np.float) / 3.,
-               "d": np.array([1, 1, 0, 1], dtype=np.float) / 3.,
-               "x": np.array([1, 1, 1, 1], dtype=np.float) / 4.,
-               "n": np.array([1, 1, 1, 1], dtype=np.float) / 4.,
-               ".": np.array([0, 0, 0, 0], dtype=np.float),
-               "-": np.array([0, 0, 0, 0], dtype=np.float)
-              }
+    convert = {
+        "a": np.array([1, 0, 0, 0], dtype=np.float),
+        "t": np.array([0, 1, 0, 0], dtype=np.float),
+        "u": np.array([0, 1, 0, 0], dtype=np.float),
+        "c": np.array([0, 0, 1, 0], dtype=np.float),
+        "g": np.array([0, 0, 0, 1], dtype=np.float),
+        "k": np.array([0, 1, 0, 1], dtype=np.float) / 2.0,
+        "m": np.array([1, 0, 1, 0], dtype=np.float) / 2.0,
+        "r": np.array([1, 0, 0, 1], dtype=np.float) / 2.0,
+        "y": np.array([0, 1, 1, 0], dtype=np.float) / 2.0,
+        "s": np.array([0, 0, 1, 1], dtype=np.float) / 2.0,
+        "w": np.array([1, 1, 0, 0], dtype=np.float) / 2.0,
+        "b": np.array([0, 1, 1, 1], dtype=np.float) / 3.0,
+        "v": np.array([1, 0, 1, 1], dtype=np.float) / 3.0,
+        "h": np.array([1, 1, 1, 0], dtype=np.float) / 3.0,
+        "d": np.array([1, 1, 0, 1], dtype=np.float) / 3.0,
+        "x": np.array([1, 1, 1, 1], dtype=np.float) / 4.0,
+        "n": np.array([1, 1, 1, 1], dtype=np.float) / 4.0,
+        ".": np.array([0, 0, 0, 0], dtype=np.float),
+        "-": np.array([0, 0, 0, 0], dtype=np.float),
+    }
     return np.vstack((convert[base] for base in sequence.lower()))
 
 
@@ -142,7 +179,9 @@ def fasta_to_contigs(path, return_headers=False):
 
     def add_contig(header, seq):
         if len(seq) == 0:
-            raise Exception("Attempted to add a contig with length 0. Not normal! Path is " + path)
+            raise Exception(
+                "Attempted to add a contig with length 0. Not normal! Path is " + path
+            )
         if sequence_contains_invalid(seq):
             raise Exception("Sequence contains invalid characters.")
         contigs.append(seq.lower())
@@ -155,7 +194,7 @@ def fasta_to_contigs(path, return_headers=False):
             # New contig starting
 
             # Save current sequence and flush buffer
-            if len(buffer) > 0.:
+            if len(buffer) > 0.0:
                 add_contig(header, buffer)
                 buffer = ""
 
@@ -188,26 +227,28 @@ def reverse_complement(sequence):
         raise ValueError("The sequence contains invalid nucleotides.")
 
     sequence = sequence.lower()
-    complement = {'a': 't',
-                  'c': 'g',
-                  'g': 'c',
-                  't': 'a',
-                  'k': 'm',
-                  'm': 'k',
-                  'r': 'y',
-                  'y': 'r',
-                  's': 's',
-                  'w': 'w',
-                  'b': 'v',
-                  'v': 'b',
-                  'h': 'd',
-                  'd': 'h',
-                  'x': 'n',
-                  'n': 'n',
-                  '.': '.',
-                  '-': '-',
-                  NON_STANDARD_MARKER: NON_STANDARD_MARKER}
-    return ''.join([complement[base] for base in sequence[::-1]])
+    complement = {
+        "a": "t",
+        "c": "g",
+        "g": "c",
+        "t": "a",
+        "k": "m",
+        "m": "k",
+        "r": "y",
+        "y": "r",
+        "s": "s",
+        "w": "w",
+        "b": "v",
+        "v": "b",
+        "h": "d",
+        "d": "h",
+        "x": "n",
+        "n": "n",
+        ".": ".",
+        "-": "-",
+        NON_STANDARD_MARKER: NON_STANDARD_MARKER,
+    }
+    return "".join([complement[base] for base in sequence[::-1]])
 
 
 def sequence_contains_non_standard(sequence):
@@ -224,7 +265,9 @@ def sequence_contains_invalid(sequence):
 
     """
     valid = dict(zip(NON_STANDARD_NUCLEOTIDES, [True] * len(NON_STANDARD_NUCLEOTIDES)))
-    valid.update({"a": True, "c": True, "g": True, "t": True, NON_STANDARD_MARKER: True})
+    valid.update(
+        {"a": True, "c": True, "g": True, "t": True, NON_STANDARD_MARKER: True}
+    )
     for x in sequence.lower():
         if x not in valid:
             return True
